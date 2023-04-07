7084698086210ae9e06be1eef42f8875c78c4c56
Gianni De vadder <giannidevadder0@gmail.com>
2023-04-07 11:25:08 +0200

    Moved Classes into utils/classes.py and add an argument after dataset in execution command, true for use Cprofile

--- optimized.py
+++ optimized.py
@@ -1,45 +1,10 @@
-from bruteforce import InvestmentOptimizer, Share
-import csv
+from utils.classes import InvestmentOptimizerGreedy
+import sys, cProfile
 
+invesment = InvestmentOptimizerGreedy(500, sys.argv[1])
 
-class InvestmentOptimizerGreedy:
-    def __init__(self, max_invest, shares_file):
-        self.max_invest = max_invest
-        self.shares_list = self.read_csv(shares_file)
-
-    def read_csv(self, file_path):
-        """Import shares data from a CSV file
-
-        @param file_path: path to the CSV file
-        @return: shares data (list of Share objects)
-        """
-        shares_list = []
-        with open(file_path) as csvfile:
-            shares_file = csv.reader(csvfile, delimiter=",")
-            for row in shares_file:
-                name, price, profit = row
-                shares_list.append(Share(name, float(price), float(profit)))
-        return shares_list
-
-    def set_combos(self):
-        """Get the most profitable combination of shares for the investment limit"""
-        shares_sorted = sorted(self.shares_list, key=lambda share: -share.profit)
-        combo = []
-        total_cost = 0
-        total_profit = 0
-        for share in shares_sorted:
-            if total_cost + share.price <= self.max_invest:
-                combo.append(share)
-                total_cost += share.price
-                total_profit += share.price * share.profit / 100
-        return combo
-
-    def optimize_investment(self):
-        """Main function to optimize the investment"""
-        print(f"\nProcessing {len(self.shares_list)} shares for {self.max_invest}€ :")
-        best_combo = self.set_combos()
-        InvestmentOptimizer.display_results(best_combo)
-
-
-invesment = InvestmentOptimizerGreedy(500, "actions.csv")
-print(invesment.optimize_investment())
+print(len(sys.argv))
+if len(sys.argv) >= 3 and sys.argv[2] == "true":
+    cProfile.run("invesment.optimize_investment()", sort="tottime")
+else:
+    invesment.optimize_investment()
---
+++ utils/classes.py
@@ -0,0 +1,134 @@
+import csv
+import itertools
+
+
+class Share:
+    def __init__(self, name, price, profit):
+        self.name = name
+        self.price = price
+        self.profit = profit
+
+    def __repr__(self):
+        return f"{self.name} ({self.price} €, {self.profit} %)"
+
+
+class InvestmentOptimizer:
+    def __init__(self, max_invest, shares_file):
+        self.max_invest = max_invest
+        self.shares_list = self.read_csv(shares_file)
+
+    def read_csv(self, file_path):
+        """Import shares data from a CSV file
+
+        @param file_path: path to the CSV file
+        @return: shares data (list of Share objects)
+        """
+        shares_list = []
+        with open(file_path) as csvfile:
+            shares_file = csv.reader(csvfile, delimiter=",")
+            for row in shares_file:
+                name, price, profit = row
+                shares_list.append(Share(name, float(price), float(profit)))
+        return shares_list
+
+    def set_combos(self):
+        """Set all possible combinations of shares
+        Check if under max possible investment
+        Check and get highest profit
+
+        @return: most profitable combination (list of Share objects)
+        """
+        profit = 0
+        best_combo = []
+
+        for i in range(len(self.shares_list)):
+            combos = itertools.combinations(self.shares_list, i + 1)
+
+            for combo in combos:
+                total_cost = self.calc_cost(combo)
+
+                if total_cost <= self.max_invest:
+                    total_profit = self.calc_profit(combo)
+
+                    if total_profit > profit:
+                        profit = total_profit
+                        best_combo = combo
+
+        return best_combo
+
+    @staticmethod
+    def calc_cost(combo):
+        """Sum of current share combo prices
+
+        @param combo: list of current shares combo
+        @return: total cost (float)
+        """
+        return sum([share.price for share in combo])
+
+    @staticmethod
+    def calc_profit(combo):
+        """Sum of current share combo profit
+
+        @param combo: list of current shares combo
+        @return: total profit (float)
+        """
+        return sum([share.price * share.profit / 100 for share in combo])
+
+    @staticmethod
+    def display_results(best_combo):
+        """Display best combination results
+
+        @param best_combo: most profitable shares combination (list of Share objects)
+        """
+        print(f"\nLes plus profitables ({len(best_combo)} Actions) :\n")
+        for share in best_combo:
+            print(share)
+        print("\nCout total : ", InvestmentOptimizer.calc_cost(best_combo), "€")
+        print(
+            "Profit après 2 ans : +", InvestmentOptimizer.calc_profit(best_combo), "€"
+        )
+
+    def optimize_investment(self):
+        """Main function to optimize the investment"""
+        print(f"\nAnalyse de {len(self.shares_list)} actions pour {self.max_invest}€ :")
+        best_combo = self.set_combos()
+        InvestmentOptimizer.display_results(best_combo)
+
+
+class InvestmentOptimizerGreedy:
+    def __init__(self, max_invest, shares_file):
+        self.max_invest = max_invest
+        self.shares_list = self.read_csv(shares_file)
+
+    def read_csv(self, file_path):
+        """Import shares data from a CSV file
+
+        @param file_path: path to the CSV file
+        @return: shares data (list of Share objects)
+        """
+        shares_list = []
+        with open(file_path) as csvfile:
+            shares_file = csv.reader(csvfile, delimiter=",")
+            for row in shares_file:
+                name, price, profit = row
+                shares_list.append(Share(name, float(price), float(profit)))
+        return shares_list
+
+    def set_combos(self):
+        """Get the most profitable combination of shares for the investment limit"""
+        shares_sorted = sorted(self.shares_list, key=lambda share: -share.profit)
+        combo = []
+        total_cost = 0
+        total_profit = 0
+        for share in shares_sorted:
+            if total_cost + share.price <= self.max_invest:
+                combo.append(share)
+                total_cost += share.price
+                total_profit += share.price * share.profit / 100
+        return combo
+
+    def optimize_investment(self):
+        """Main function to optimize the investment"""
+        print(f"\nAnalyse de {len(self.shares_list)} actions pour {self.max_invest}€ :")
+        best_combo = self.set_combos()
+        InvestmentOptimizer.display_results(best_combo)
